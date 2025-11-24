5985a4644a6b69cb938173331d83b2053113e104
nibesmil <carefox1@naver.com>
2025-11-25 00:35:07 +0900

    포스트 작성 일자 업데이트

--- curatemagazine/src/components/PostCard.js
+++ curatemagazine/src/components/PostCard.js
@@ -1,9 +1,10 @@
-function PostCard({ post }) {
+function PostCard({ post, date }) {
   return (
     <div className="post-card">
       <img src={post.image} alt={post.title} />
       <h3>{post.title}</h3>
       <p className="editor">작성자 · {post.editor}</p>
+      <p className="date"> 작성일 · {date} </p>
     </div>
   );
 }
--- curatemagazine/src/components/PostList.js
+++ curatemagazine/src/components/PostList.js
@@ -1,10 +1,13 @@
 import PostCard from "./PostCard";
 
+const today = new Date();
+const formattedDate = today.toLocaleDateString("ko-KR");
+
 function PostList({ posts }) {
   return (
     <section className="post-list">
       {posts.map((post) => (
-        <PostCard key={post.id} post={post} />
+        <PostCard key={post.id} post={post} date={formattedDate} />
       ))}
     </section>
   );
