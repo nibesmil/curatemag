3425663901670784ae616dc3df1a946ed7d25e75
nibesmil <carefox1@naver.com>
2025-11-24 23:37:39 +0900

    모듈 분리 | 매거진 사이트 하단

---
+++ curatemagazine/src/components/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2025 Curatemag. All Rights Reserved.</p>
+      <p>by ansanthesoloist</p>
+    </footer>
+  );
+}
+
+export default Footer;
