e01c4724cceef0e1e40832a2d6d33d7cc71fd9fc
nibesmil <carefox1@naver.com>
2025-11-24 23:36:58 +0900

    모듈 분리 | 매거진 사이트 하단

---
+++ curatemagazine/src/components/Hero.js
@@ -0,0 +1,10 @@
+function Hero() {
+  return (
+    <section className="hero">
+      <h2>Curating Stories & Culture</h2>
+      <p>패션 | 음악 | 라이프스타일의 선두주자 (를 희망합니다.)</p>
+    </section>
+  );
+}
+
+export default Hero;
